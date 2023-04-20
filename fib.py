ae725e042b3f2b5c0f3dd97bc94cfacf01813ca0
aaron-everly <aaron.everly@hey.com>
2023-04-20 13:47:00 -0700

    first commit

---
+++ fib.py
@@ -0,0 +1,32 @@
+print("Enter a number: ")
+n = input()
+print("") # spacing
+print("sequence or number? (s/n): ")
+c = input()
+print("") # spacing
+
+
+def fib(n):
+    a = 0
+    b = 1
+    value = 0
+
+    n = int(n)
+    if c == "s":
+        print(a)
+        print(b)
+
+    for i in range(2, n + 1):
+        value = a + b
+        a = b
+        b = value
+
+        if c == "s":
+            print(value)
+    
+    if c == "n":
+        n = str(n)
+        value = str(value)
+        print("Fib of " + n + " is: " + value)
+
+fib(n)
